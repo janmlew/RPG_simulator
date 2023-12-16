4ca18feb81c54b6488709d380887da43ab8a6cb7
janmlew <janmikolajlewinski@gmail.com>
2023-12-17 00:43:24 +0100

    Added roll history management system and updated the dice roll system.

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from warhammer40k import Creature
 
 creature = Creature(name="Ktul")
 
-print(creature.roll(dice_number=1, dice_sides=100), creature.roll(2, 6))
+print(creature.roll(dice_number=1, dice_sides=100))
+print(creature.roll(2, 6))
+print(creature.roll_history)
--- warhammer40k.py
+++ warhammer40k.py
@@ -10,10 +10,14 @@ class Creature:
         self.name = name
         self.kind = kind
         self.level = level
+        self.rolls = []
 
     def roll(self, dice_number=1, dice_sides=6):
-        rolls = []
         for i in range(0, dice_number):
-            rolls.append(np.random.randint(1, dice_sides + 1))
-            print(f"{self.name} rolled {rolls[-1]}.")
-        return rolls
+            self.rolls.append(np.random.randint(1, dice_sides + 1))
+            print(f"{self.name} rolled {self.rolls[-1]}.")
+        return self.rolls[-1+dice_number:]
+
+    @property
+    def roll_history(self):
+        return self.rolls
