452b782d3bfb5b64a0245eb4c936fcbbe536a6af
janmlew <janmikolajlewinski@gmail.com>
2025-01-26 21:27:06 +0100

    New code + small fixes in variable names.

---
+++ Wrath and Glory.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+
+def throw_dice(number_of_dice: int = 1, sides: int = 6, difficulty: int = 0):
+    """
+    :param number_of_dice: How many dice to dice_list.
+    :param sides: Defaults to a 6-sided die.
+    :param difficulty: Usual difficulty is ST = 3.
+    :return: Returns a list of thrown dice.
+    """
+    dice_list: list = []
+    dice_list = [np.random.randint(1, sides+1) for die in range(1, number_of_dice+1)]
+    for die in dice_list:
+        if die == 6:
+    return dice_list
+
+
+def analyze_throw(dice_list: list, st:int = 0):
+    icon_list= []
+    wrath_result = "Emperor protects!"
+    shifts_count = 0
+    for die in dice_list:
+        if die == 6:
+            icon_list.append(2)
+            shifts_count += 1
+        elif die > 3:
+            icon_list.append(1)
+    if icon_list[0] == 1:
+        wrath_result = "Complication!"
+    elif icon_list[0] == 6:
+        wrath_result = "Critical!"
+    for icon in
+    print(f"Sum = {sum(icon_list)}, ")
+
+
+
+print(throw_dice(6))
--- probability tests.py
+++ probability tests.py
@@ -1,26 +1,26 @@
 import numpy as np
 import pandas as pd
 
-dies = 1
+dice = 1
 difficulty = 0
 throws = 100
 successes: int = 0
 columns = ["Wrath"]
 
-print("Type in the number of dies (a singular int value, please):")
+print("Type in the number of dice (a singular int value, please):")
 try:
-    dies = int(input())
+    dice = int(input())
 except ValueError:
-    dies = 1
-print(f"Number of dies is {dies}.")
+    dice = 1
+print(f"Number of dice is {dice}.")
 
-for die in range(2, dies+1):
+for die in range(2, dice+1):
     columns.append(f"Die {die}")
 
 df = pd.DataFrame(columns=columns)
 
 for throw in range(0, throws):
-    for die in range(1, dies+1):
+    for die in range(1, dice+1):
         toss = np.random.randint(1, 7)
         df.loc[throw, columns[die-1]] = toss
         if toss == 6:
